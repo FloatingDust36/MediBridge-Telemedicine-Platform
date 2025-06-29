c3990ceeab2c7984216d1826a7fdfde7ee8641fb
Jasner13 <tindoganjesnar@gmail.com>
2025-06-29 23:41:28 +0800

    Debugged the Database

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -101,7 +101,7 @@ const handleGoogleRegister = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: window.location.origin, // Redirects back to your app after Google login
+        redirectTo: `${window.location.origin}/oauth-callback`, // Redirects back to your app after Google login
       },
     });
 
